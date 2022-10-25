03a6ca6a63e7f14971579692e898a292fc76ec46
Raph2954 <raphman8@gmail.com>
2022-10-25 15:20:54 +0100

    upload

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'f75kb6j1gi';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-6yioft-i.us.auth0.com',
+  clientId: 'spAdH9qZUuUd1X0BX9aKj4bg29KoLhyu',
+  callbackUrl: 'http://localhost:3000/callback'
+};
